40017b99763bd9f9e7fdc911a014a672f6324391
eric polley <ericpolley@live.com>
2022-11-25 11:35:38 -0800

    cosmetic fix gamepad

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Tutorials() {
   return (
     <div id="Tutorials" className="hide" >
-      <iframe  src="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PLce5_YZyrUw6mOXubpKKWoTROUqCfIDDo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <iframe  className="youtubeiframe" src="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PLce5_YZyrUw6mOXubpKKWoTROUqCfIDDo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
